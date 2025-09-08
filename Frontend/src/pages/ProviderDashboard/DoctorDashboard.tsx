71b2d660d062e02d27c892d550b54a2d29f5f7fa
JosephOkumu <jokumu25@gmail.com>
2025-09-08 23:58:43 +0300

    http://localhost:8080/patient-dashboard/lab-provider/1?test=1

--- Frontend/src/pages/ProviderDashboard/DoctorDashboard.tsx
+++ Frontend/src/pages/ProviderDashboard/DoctorDashboard.tsx
@@ -1037,8 +1037,8 @@ const DoctorDashboard = () => {
         <div className="mb-8 flex flex-col md:flex-row md:items-center md:justify-between">
           <div>
             <h1 className="text-3xl font-bold">
-              <span className="text-primary-blue">AFYA</span>
-              <span className="text-secondary-green"> MAWINGUNI</span>
+              <span className="text-primary-blue">ACESO</span>
+              <span className="text-secondary-green"> HEALTH SOLUTIONS</span>
             </h1>
             <p className="text-gray-600 mt-2">Doctor's Dashboard</p>
           </div>
