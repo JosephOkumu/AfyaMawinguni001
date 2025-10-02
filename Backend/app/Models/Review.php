11110b6aef08dff159e979bdc847329f14b7195a
JosephOkumu <jokumu25@gmail.com>
2025-10-03 01:26:25 +0300

    refactor: modify code

--- Backend/app/Models/Review.php
+++ Backend/app/Models/Review.php
@@ -10,7 +10,9 @@ class Review extends Model
     protected $fillable = [
         'patient_id',
         'doctor_id', 
+        'nursing_provider_id',
         'appointment_id',
+        'nursing_service_id',
         'rating',
         'review_text'
     ];
@@ -32,5 +34,15 @@ class Review extends Model
     public function appointment(): BelongsTo
     {
         return $this->belongsTo(Appointment::class, 'appointment_id');
+    }
+
+    public function nursingProvider(): BelongsTo
+    {
+        return $this->belongsTo(NursingProvider::class, 'nursing_provider_id');
+    }
+
+    public function nursingService(): BelongsTo
+    {
+        return $this->belongsTo(NursingService::class, 'nursing_service_id');
     }
 }
